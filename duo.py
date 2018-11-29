496a664ad91090f09fd0dea08789aead62242d55
YDyiduo <45254095+YDyiduo@users.noreply.github.com>
2018-11-29 17:07:10 +0800

    Add files via upload

---
+++ duo.py
@@ -0,0 +1,12 @@
+file_path = r'C:\Users\Administrator\Desktop\root\dir1\cp4_find_max.c'
+# try:
+# 	f = open(file_path)
+# except:
+# 	print("no such file or directory!")
+# finally:
+# 	f.close()
+# 	print("file closed!")
+
+
+with open(file_path) as f:
+		pass
